0b02e02464bada170a88eee39216475f5b8038b4
preetisilla <preetisilla18@gmail.com>
2026-04-13 23:19:47 +0530

    TypeScript

---
+++ Chapter16_TypeScript/1_TS1.js
@@ -0,0 +1,7 @@
+"use strict";
+let testName = "TypeScript Basics";
+console.log(testName);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 10));
---
+++ Chapter16_TypeScript/1_TS1.ts
@@ -0,0 +1,8 @@
+let testName : string = "TypeScript Basics";
+console.log(testName);
+
+function add(a:number , b:number) :number {
+    return a+b;
+}
+
+console.log(add(5,10));
